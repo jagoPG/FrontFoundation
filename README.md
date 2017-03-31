304461ca34ab52c35a5d1ab5d18412f2e8b3768c
benatespina <benatespina@gmail.com>
2017-03-31 14:03:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# LIN3S Front Foundation
+> Base CSS and JS files for building views in LIN3S way.
+
+## Installation
+The recommended and the most suitable way to install is through *Yarn*:
+```bash
+$ yarn add lin3s-front-foundation
+```
+or alternatively through *NPM*:
+```bash
+$ npm install --save lin3s-front-foundation
+```
+Also, you can install through *Bower*.
+```bash
+$ bower install --save lin3s-front-foundation
+```
---
+++ js/atoms/parsley-form-error.js
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the Front Foundation package.
+ *
+ * Copyright (c) 2017-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+
+'use strict';
+
+import Parsley from 'parsleyjs';
+import 'parsleyjs/dist/i18n/en';
+import 'parsleyjs/dist/i18n/es';
+
+const getLang = (lang) => {
+  if (null === lang) {
+    return lang;
+  }
+
+  return document.getElementsByTagName('html')[0].getAttribute('lang');
+};
+
+const initParsley = (lang = null) => {
+  const dividedLang = getLang(lang).split('_');
+
+  let locale = lang;
+  if (dividedLang.length > 0) {
+    locale = dividedLang[0];
+  }
+
+  Parsley.setLocale(locale);
+};
+
+export default initParsley;
